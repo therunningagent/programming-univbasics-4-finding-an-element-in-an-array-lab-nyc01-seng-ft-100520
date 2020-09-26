e4b69011bdb8c51c4136e9de6807a4b416a68aea
Luis Torres <luistjr@gmail.com>
2020-09-26 20:36:41 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -10,11 +10,10 @@ def find_element_index(array, value_to_find)
     puts array[count] == value_to_find
     
     found_value_index = count 
-    
 end 
   count += 1
 end
-found_value_index
+puts found_value_index
 end 
 
 find_element_index(array, 4)
